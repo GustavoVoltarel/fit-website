837948c2c9af1907ab0c833b0dc5c69f4968162d
Gustavo Voltarel Deodato da Silva <gustavo.voltarel@santacasasc.local>
2021-11-29 12:32:30 -0300

    Finish

--- js/script.js
+++ js/script.js
@@ -16,8 +16,52 @@ var swiper = new Swiper('.home-slider', {
     effect: "fade",
     gradCursor: true,
     loop: true,
+    centeredSlides:true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+})
+
+var swiper = new Swiper('.review-slider', {
+    spaceBetween: 20,
+    gradCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView:1,
+        },
+        600: {
+            slidesPerView:2,
+        }
+    }
+})
+
+var swiper = new Swiper('.blogs-slider', {
+    spaceBetween: 20,
+    gradCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView:1,
+        },
+        768: {
+            slidesPerView:2,
+        },
+        991: {
+            slidesPerView:3,
+        }
+    }
 })
